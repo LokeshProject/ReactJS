3d6a2ad2e0d0619c79b187413279debf7cdfdca1
Lokesh <lokesh.das@maventechlabs.com>
2025-07-23 11:25:10 +0530

    Basic React Complite

--- 03tailwindprops/src/App.jsx
+++ 03tailwindprops/src/App.jsx
@@ -1,16 +1,25 @@
-
-import './App.css'
+import "./App.css";
+import Card from "./component/Card";
 
 function App() {
 
 
+  const myObject = {
+    name: "Lokesh Das",
+    age: 25,
+    city: "Kolkata",
+    country: "India",
+  }
+  const myArray = [1, 2, 3, 4, 5];
   return (
     <>
-     <h1 className="text-3xl font-bold underline text-blue-500">
+      <h1 className="text-3xl font-bold underline text-blue-500 mb-4 ">
         Lokesh Das
       </h1>
+      <Card channel='Lokesh' objpass={myObject} arrpass={myArray}/>
+      <Card />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
