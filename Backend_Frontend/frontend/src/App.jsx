47e2a9120f04019135e2d664357853b7ae38b1f2
Lokesh <lokesh.das@maventechlabs.com>
2025-07-25 16:04:49 +0530

    ...

--- Backend_Frontend/frontend/src/App.jsx
+++ Backend_Frontend/frontend/src/App.jsx
@@ -7,27 +7,42 @@ import axios from "axios";
 function App() {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false); 
+  const [error, setError] = useState(false);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
+    const controller = new AbortController();
     (async () => {
       try {
         setError(false);
         setLoading(true);
-        const response = await axios.get("/api/products");
+        const response = await axios.get("/api/products?search=" + searchTerm, {
+          signal: controller.signal,
+        });
         setProducts(response.data);
         setLoading(false);
       } catch (error) {
+        if (axios.isCancel(error)) {
+          console.log("Request cancelled", error.message);
+          return;
+        } 
+
         setError(true);
         setLoading(false);
         console.log("Response data:", response.data);
       }
     })();
-  }, []);
+
+    return () => {
+      controller.abort();
+    }
+  }, [searchTerm]);
+
+  
   if (loading) {
     return <h1>Loading...</h1>;
   }
-  if (error) {  
+  if (error) {
     return <h1>Error occurred while fetching data</h1>;
   }
 
@@ -35,6 +50,15 @@ function App() {
     <>
       <h1>React and API</h1>
       <h2>number of product are:{products.length}</h2>
+      <input
+        type="text"
+        placeholder="Search product"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error occurred while fetching data</h1>}
     </>
   );
 }
